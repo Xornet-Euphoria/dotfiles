037511562783cd93cf9234ce2e5bad93ea68027f
Xornet-Euphoria <einnotwendigesubel@gmail.com>
2019-12-18 15:53:02 +0900

    mac対応

--- setup.sh
+++ setup.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 DOT_FILES=(
-    .gitconfig
+    .gitconfig .zshrc
 )
 
 for file in ${DOT_FILES[@]}
 do
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
+
+if [ "$(uname)" == 'Darwin' ]; then
+  echo 'aliases for MacOS'
+  ln -s $HOME/dotfiles/.aliases_mac $HOME/.aliases
+elif [ "$(expr substr (uname -s ) 1 5)" == 'Linux' ]; then
+  echo 'aliases for Linux'
+  ln -s $HOME/dotfiles/.aliases $HOME/.alias
+fi
