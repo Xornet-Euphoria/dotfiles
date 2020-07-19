a5e9e85a6ffe63693d06139b2d9df9f7f70ec77a
Xornet-Euphoria <einnotwendigesubel@gmail.com>
2020-07-20 03:02:45 +0900

    revise setup.fish

--- setup.fish
+++ setup.fish
@@ -1,4 +1,4 @@
 for file in (l .config/fish/functions -1 | grep -v /)
-    ln -fs $HOME/dotfiles/.config/fish/functions$file $HOME/$file
+    ln -fs $HOME/dotfiles/.config/fish/functions/$file $HOME/.config/fish/functions/$file
 end
 
